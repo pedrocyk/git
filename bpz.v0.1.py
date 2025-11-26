b2ab877cf4dea7830c8d457a06cb2bc7c2a224a5
pedro <pedro1221cyk@gmail.com>
2025-11-26 13:14:14 -0300

    docs: adiciona comentários explicativos em todo o programa

--- bpz.v0.1.py
+++ bpz.v0.1.py
@@ -1,14 +1,20 @@
-from time import sleep
-from random import randint
+from time import sleep  # Importa a função sleep para pausas no programa
+from random import randint  # Importa randint para gerar números aleatórios
 
-opçoes=opc=opx=0
+# Variáveis que controlam os menus
+opçoes = opc = opx = 0
 
+# Loop principal do programa (menu principal)
 while True:
     print('=' * 30)
+    # Título formatado com cores ANSI
     print(f'{'\033[1:35mBIBLIOTECA PYTHON\033[m':^40}\n'
           f'{'\033[3mDo Pedro\033[m':^35}')
     print('=' * 30)
+
     print('\033[3mBem vindo a sua Biblioteca Python\033[m')
+
+    # Menu principal
     print('''[1] aula 17 \033[1mlistas\033[m
 [2] aula 16 \033[1mtuplas\033[m
 [3] aula 15
@@ -16,90 +22,125 @@ while True:
 [5] sair
 ''')
 
+    # Tratamento de erro para garantir que o usuário digite um número
     try:
         opçoes = int(input('Qual item deseja relembrar?'))
         print('~' * 30)
     except ValueError:
-        opçoes= int (input('\033[1:31mErro\033[m...\nQual item deseja relembrar?'))
-    if opçoes==5:
+        opçoes = int(input('\033[1:31mErro\033[m...\nQual item deseja relembrar?'))
+
+    # Opção para sair do programa
+    if opçoes == 5:
         break
-    if opçoes==4:
+
+    # ==================== MENU DE FUNÇÕES ====================
+    if opçoes == 4:
         while True:
-            print ('[1] localização\n'
-                   '[2] contagem \033[1:35mlen\033[m\n'
-                   '[3] inverção ')
-            if opc not in range (1,4):
-                opc = int (input ('Qual função quer visualizar?'))
+            print('[1] localização\n'
+                  '[2] contagem \033[1:35mlen\033[m\n'
+                  '[3] inverção ')
+
+            # Garante que a opção seja válida
+            if opc not in range(1, 4):
+                opc = int(input('Qual função quer visualizar?'))
+
+            # Opção 1 - Localização
             if opc == 1:
-                print ('-'*30)
-                print ('Para localizar um item ')
-            if opc ==2:
-                print ('-'*30)
-                print ('Para realizar uma contagem usa-se \033[1:35mlen\033[m\n\033[1mExemplo\033[m:')
-                listaop4= ['Sebastian','Jorge', 'Cristovam''e','josépi']
+                print('-' * 30)
+                print('Para localizar um item ')
+
+            # Opção 2 - Contagem com len()
+            if opc == 2:
+                print('-' * 30)
+                print('Para realizar uma contagem usa-se \033[1:35mlen\033[m\n\033[1mExemplo\033[m:')
+
+                # Lista de exemplo
+                listaop4 = ['Sebastian', 'Jorge', 'Cristovam''e', 'josépi']
+
+                # Mostra os valores da lista
                 for c in listaop4:
-                    print (f'{c}',end=' ')
-                intopc2 = str (input ('\nDigite S para ver como funciona')).lower().strip()[0]
-                if intopc2=='s':
-                    print ('_'*30)
-                    print ('''listaop4= ['Sebastian','Jorge', 'Cristovam''e','josépi']
+                    print(f'{c}', end=' ')
+
+                # Pergunta se o usuário quer ver como funciona
+                intopc2 = str(input('\nDigite S para ver como funciona')).lower().strip()[0]
+
+                if intopc2 == 's':
+                    print('_' * 30)
+                    print('''listaop4= ['Sebastian','Jorge', 'Cristovam''e','josépi']
                     for c in listaop4:
                         print (f'{c}',end=' ')''')
-                    contin=str (input ('\nQuer ver outro exemplo? [sim/não]')).strip().lower()[0]
+
+                    contin = str(input('\nQuer ver outro exemplo? [sim/não]')).strip().lower()[0]
                     if contin == 'n':
                         break
 
-    if opçoes==3:
-        print ('Em obras...')
+    # ==================== OPÇÃO EM OBRAS ====================
+    if opçoes == 3:
+        print('Em obras...')
         break
 
+    # ==================== MENU DE TUPLAS ====================
     if opçoes == 2:
         print('\033[4:34mtuplas\033[m')
         print('As tuplas são variáveis compostas e \033[1:34mimutáveis\033[m que permitem armazenar')
-        print('\033[1:34mvários\033[m valores em uma mesma estrutura, acessíveis por \033[1:34mchaves individuais\033[m{}.\n\033[1mExemplos\033[m:')
+        print(
+            '\033[1:34mvários\033[m valores em uma mesma estrutura, acessíveis por \033[1:34mchaves individuais\033[m{}.\n\033[1mExemplos\033[m:')
+
         while True:
             opç = int(input('[1] Números por extenso.\n'
                             '[2] Maior e menor/random\n'
                             '[3] Localização em tuplas'))
+
+            # Opção 1 - Números por extenso
             if opç == 1:
                 tupla = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
                          'onze', 'doze',
                          'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
                 resp = int(input('digite um valor entre 0 e 20:'))
                 print(f'\033[34m{tupla[resp]}\033[m')
-                exe1 = int(input('Para ver o exercício digite 0 ou se não 1: '))
 
-                print ('-'*30)
+                exe1 = int(input('Para ver o exercício digite 0 ou se não 1: '))
+                print('-' * 30)
+
+                # Mostra o código do exercício
                 if exe1 == 0:
                     print('''[tupla = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
                          'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 resp = int(input('digite um valor entre 0 e 20:'))
 print(f'{tupla[resp]}')''')
 
+            # Opção 2 - Maior e menor valor da tupla
             if opç == 2:
                 tuplas = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
                 print(f'O maior número sorteado foi {max(tuplas)}')
                 print(f'O menor número sorteado foi {min(tuplas)}')
+
                 perg = str(input('Deseja ver outro exemplo? SIM/NÃO')).lower().strip()[0]
-                print('-'*20)
+                print('-' * 20)
                 if perg == 'n':
                     break
 
-            if opç==3:
+            # Opção 3 - Contagem e localização dentro da tupla
+            if opç == 3:
                 cont = 0
                 t = (int(input('digite um valor: ')),
                      int(input('Digite outro valor: ')),
                      int(input('Digite mais um valor: ')),
                      int(input('digite o ultimo valor ')))
+
                 print(f'9 apareceu {t.count(9)} vezes')
+
+                # Verifica se o número 3 existe na tupla
                 if 3 in t:
                     print(f'O valor 3 está na {t.index(3) + 1}° posição ')
                 else:
                     sleep(0.5)
                     print('\033[1:31mAcabou!!\033[m')
+
                 exe1 = int(input('Para ver o exercício digite 0 ou se não 1: '))
                 if exe1 == 0:
-                    print ('''cont = 0
+                    print('''cont = 0
     t = (int(input('digite um valor: ')),
      int(input('Digite outro valor: ')),
      int(input('Digite mais um valor: ')),
@@ -110,50 +151,64 @@ print(f'{tupla[resp]}')''')
     else:
         sleep(0.5)
     print('\033[1:31mAcabou!!\033[m')''')
+
                 perg = str(input('Deseja ver outro exemplo? SIM/NÃO')).lower().strip()[0]
                 if perg == 'n':
                     break
 
-    if opçoes ==1:
-            print('\033[4:31mListas\033[m')
-            print('As Listas são variáveis compostas e \033[1:31mmutáveis\033[m diferente das tuplas, que permitem armazenar')
-            print('\033[1:31mvários\033[m valores em uma mesma estrutura, acessíveis por \033[1:31mchaves individuais\033[m[].\n\033[1mExemplos\033[m:')
-            while True:
+    # ==================== MENU DE LISTAS ====================
+    if opçoes == 1:
+        print('\033[4:31mListas\033[m')
+        print(
+            'As Listas são variáveis compostas e \033[1:31mmutáveis\033[m diferente das tuplas, que permitem armazenar')
+        print(
+            '\033[1:31mvários\033[m valores em uma mesma estrutura, acessíveis por \033[1:31mchaves individuais\033[m[].\n\033[1mExemplos\033[m:')
 
-                opx = int(input('[1] como remover um objeto da lista\n'
-                        '[2] formas de remover um objeto'))
-                print('_' * 30)
-                if opx == 1:
-                    print ('Exemplo: ')
-                    lista= [randint (0,5),
-                    randint (0,5),
-                    randint (0,5),
-                    randint (0,5)]
-                    if 5 in lista:
-                        lista.remove(5)
-                    print (f'foram lidos {len(lista)} valores da lista:\n{lista}')
-                whilecont=str (input ('Deseja ver outro exemplo? SIM/NÃO')).lower().strip()[0]
-                if whilecont=='n':
-                    break
-                if opx==2:
-                    print ('na lista existem 3 metodos: 1° \033[1:31mdel lista[0]\033[m (sem espaço) 2° \033[1:31mlista.pop(0)\033[m\ne a 3° que consiste em excluir um objeto' 
-' espefico da lista, sendo ele \033[1:31mlista.remove()\033[m em casos de escassez do item utiliza se o if')
-                    exemplo= str (input ('Ver como funciona? [S/N]')).lower().strip()[0]
-                    if exemplo== 's':
-                        print ('''lista= [1,2,3,4]
+        while True:
+            opx = int(input('[1] como remover um objeto da lista\n'
+                            '[2] formas de remover um objeto'))
+            print('_' * 30)
+
+            # Remoção com remove
+            if opx == 1:
+                print('Exemplo: ')
+                lista = [randint(0, 5),
+                         randint(0, 5),
+                         randint(0, 5),
+                         randint(0, 5)]
+
+                if 5 in lista:
+                    lista.remove(5)
+
+                print(f'foram lidos {len(lista)} valores da lista:\n{lista}')
+
+            whilecont = str(input('Deseja ver outro exemplo? SIM/NÃO')).lower().strip()[0]
+            if whilecont == 'n':
+                break
+
+            # Outras formas de remover itens
+            if opx == 2:
+                print(
+                    'na lista existem 3 metodos: 1° \033[1:31mdel lista[0]\033[m (sem espaço) 2° \033[1:31mlista.pop(0)\033[m\ne a 3° que consiste em excluir um objeto'
+                    ' espefico da lista, sendo ele \033[1:31mlista.remove()\033[m em casos de escassez do item utiliza se o if')
+
+                exemplo = str(input('Ver como funciona? [S/N]')).lower().strip()[0]
+                if exemplo == 's':
+                    print('''lista= [1,2,3,4]
 a= (f'{lista.append(10)}')
 R: [1, 2, 3, 4, 10] ''')
-                whilecont= str (input ('ver outro exemplo? [S/N]')).lower().strip()[0]
-                if whilecont=='n':
-                    break
 
+            whilecont = str(input('ver outro exemplo? [S/N]')).lower().strip()[0]
+            if whilecont == 'n':
+                break
 
     continuar = str(input('Deseja continuar com o estudo? [S/N] ')).lower()
     if continuar == 'n':
         break
 
-print ('\033[1mFinalizando...\033[m')
-sleep (1)
-print ('Biblioteca finalizada, tenha um bom dia!!!')
-#organização para separar melhor os codigos um dos outros
-#fazer comentarios grandes para eu entender o codigo
+print('\033[1mFinalizando...\033[m')
+sleep(1)
+print('Biblioteca finalizada, tenha um bom dia!!!')
+
+# organização para separar melhor os codigos um dos outros
+# fazer comentarios grandes para eu entender o codigo
